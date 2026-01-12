e3f77759270a546146f25c390267b5b06adac3b3
GlibMiasoiedov <gmiasoiedov@gmail.com>
2026-01-12 13:41:49 +0100

    Switch to local react-chessboard v5.8.6 fixed

---
+++ ui/local_modules/react-chessboard-source/react-chessboard-5.8.6/.storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import remarkGfm from 'remark-gfm';
+
+const config: StorybookConfig = {
+  stories: ['../docs/**/*.mdx', '../docs/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    'storybook-addon-deep-controls',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  staticDirs: ['../docs/assets', '../docs/stockfish'],
+  docs: {
+    docsMode: true,
+  },
+};
+export default config;
